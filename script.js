6019e232aa8b71b7cc797ddc8a33013a3ca4af2e
dencioras1 <denaskh@gmail.com>
2025-01-15 14:22:30 +0100

    Wrote down function for geting player choice. Additional 'rounds' variable added, with a value of 5. Added empty while loop in 'playGame()' function.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log('Hello, world!')
 
 // Variables for tracking the scores for the player/computer.
+let rounds = 5;
 let humanScore = 0;
 let computerScore = 0;
 
@@ -12,7 +13,8 @@ function getComputerChoice() {
 // Displays a prompt window, during which the player must input their choice between rock, paper and scissors.
 // Works based on case-insensitive string input.
 function getHumanChoice() {
-
+    let humanChoice = prompt("Enter your choice: (rock / paper / scissors)");
+    return humanChoice.toLowerCase();
 }
 
 // Controls the way each round is played.
@@ -23,6 +25,10 @@ function playRound(humanChoice, computerChoice) {
 // Controls how the entire game is played.
 // Hardcoded to have a player play 5 rounds against the computer.
 function playGame() {
+    while (rounds > 1) {
 
+
+        rounds--;
+    }
 }
 
