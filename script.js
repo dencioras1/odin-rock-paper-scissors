3d13a0c727cdee81c3f88159d570741d4e4244a7
dencioras1 <denaskh@gmail.com>
2025-01-15 13:52:12 +0100

    Written down empty functions and variables that are required. Comments added to make note of any constraints or extra information.

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log('Hello, world!')
+
+// Variables for tracking the scores for the player/computer.
+let humanScore = 0;
+let computerScore = 0;
+
+// Randomly returns a choice between rock, paper and scissors.
+function getComputerChoice() {
+
+}
+
+// Displays a prompt window, during which the player must input their choice between rock, paper and scissors.
+// Works based on case-insensitive string input.
+function getHumanChoice() {
+
+}
+
+// Controls the way each round is played.
+function playRound(humanChoice, computerChoice) {
+
+}
+
+// Controls how the entire game is played.
+// Hardcoded to have a player play 5 rounds against the computer.
+function playGame() {
+
+}
+
